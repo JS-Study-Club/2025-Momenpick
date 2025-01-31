8fd133590eb458ae1dc2692a856411fa814b4e8d
kimjihye058 <s2321@e-mirim.hs.kr>
2025-02-01 02:58:48 +0900

    [feat] : selectFrame 화면 디자인 구현

--- src/selectframe/SelectFrame.js
+++ src/selectframe/SelectFrame.js
@@ -3,7 +3,36 @@ import "./SelectFrame.css";
 
 const SelectFrame = () => {
     return(
-        <div className="selectFrame-background"></div>
+        <div className="selectFrame-background">
+            <img
+                className="designer"
+                src="/images/designerSelect.png" 
+                alt="Designer"
+                width={507}
+                height={805}
+            />
+            <img
+                className="developer"
+                src="/images/developerSelect.png" 
+                alt="Developer"
+                width={507}
+                height={805}
+            />
+            <img
+                className="teacher"
+                src="/images/teacherSelect.png" 
+                alt="Teacher"
+                width={499}
+                height={366}
+            />
+            <img
+                className="summer"
+                src="/images/summerSelect.png" 
+                alt="Summer"
+                width={499}
+                height={409}
+            />
+        </div>
     );
 };
 
