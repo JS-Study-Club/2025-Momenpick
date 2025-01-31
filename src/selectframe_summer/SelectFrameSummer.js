60029b5bf378fdf4a40b232fb4023bd70acc6b86
kimjihye058 <s2321@e-mirim.hs.kr>
2025-02-01 04:11:41 +0900

    [feat] : selectFrame_summer 화면 구현

--- src/selectframe_summer/SelectFrameSummer.js
+++ src/selectframe_summer/SelectFrameSummer.js
@@ -6,12 +6,41 @@ const SelectFrameSummer = () => {
  const navigate = useNavigate();
   return (
     <div className="select-background">
+      <div className="Select-header">
+        <img
+            src="images/backBtn.png"
+            alt="Back"
+            className="backBtn"
+            onClick={() => navigate("/SelectFrame")}
+          />
+      </div>
       <img
-          src="images/backBtn.png"
-          alt="Back"
-          className="backBtn"
-          onClick={() => navigate("/SelectFrame")}
-        />
+                className="summer1"
+                src="/images/summer1.png" 
+                alt="여름1"
+                width={432}
+                height={664}
+                onClick={() => navigate("/Summer1")}
+                style={{ cursor: "pointer" }}
+            />
+            <img
+                className="summer2"
+                src="/images/summer2.png" 
+                alt="여름2"
+                width={432}
+                height={664}
+                onClick={() => navigate("/Summer2")}
+                style={{ cursor: "pointer" }}
+            />
+            <img
+                className="summer3"
+                src="/images/summer3.png" 
+                alt="여름3"
+                width={432}
+                height={664}
+                onClick={() => navigate("/Summer3")}
+                style={{ cursor: "pointer" }}
+            />
     </div>
   );
 };
