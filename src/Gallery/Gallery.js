341ace0a1f29fd7a2805a727f2681569d7a9e414
kimjihye058 <s2321@e-mirim.hs.kr>
2025-02-04 23:56:20 +0900

    [feat] : gallery header

--- src/Gallery/Gallery.js
+++ src/Gallery/Gallery.js
@@ -1,6 +1,24 @@
+import { useNavigate } from "react-router-dom";
+import "./Gallery.css";
+
 const Gallery = () => {
+  const navigate = useNavigate();
+
     return (
-      <div>
+      <div className="gallery-background">
+        <div className="gallery-header">
+          <img
+            src="images/backBtn.png"
+            alt="Back"
+            className="backBtn"
+            onClick={() => navigate("/")}
+          />
+          <img
+            src="images/galleryLogo.png"
+            alt="GalleryLogo"
+            className="galleryLogo"
+          />
+        </div>
       </div>
     );
   };
